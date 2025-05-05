bbb746d23e1a49354c336e01a5970e35f0d14a9d
xiaojian199810 <50836202+xiaojian199810@users.noreply.github.com>
2025-05-05 15:16:01 +0800

    Update TokenBank.sol

--- week01/TokenBank.sol
+++ week01/TokenBank.sol
@@ -21,11 +21,7 @@ contract MyERC20 is ERC20 {
     }
 }
 
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-}
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract TokenBank {
 
