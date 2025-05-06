5d8e2ae21b4e2efdd04d1120e41157d4b69c0117
xiaojian199810 <50836202+xiaojian199810@users.noreply.github.com>
2025-05-06 14:22:41 +0800

    transferWithCallback

---
+++ week02/transferWithCallback/MyTokenWithCallback.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+interface TokenRecipient {
+    function tokensReceived(address sender, uint amount) external returns (bool);
+}
+
+contract MyTokenWithCallback is ERC20{
+
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply_) ERC20(name_, symbol_) {
+        _mint(msg.sender, initialSupply_);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function isContract(address _addr) public view returns (bool) {
+        if (_addr.code.length > 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    function transferWithCallback(address recipient, uint256 amount) external returns (bool) {
+
+        if (isContract(recipient)) {
+            bool rv = TokenRecipient(recipient).tokensReceived(msg.sender, amount);
+            require(rv, "No tokensReceived");
+        }
+
+        _transfer(msg.sender, recipient, amount);
+        
+        return true;
+    }
+}
