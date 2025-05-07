1ff95e38f6b37ae9cb9d44857fd5a2042c581fa5
xiaojian199810 <50836202+xiaojian199810@users.noreply.github.com>
2025-05-07 09:40:42 +0800

    transferWithCallback

--- week02/transferWithCallback/TokenBank2.sol
+++ week02/transferWithCallback/TokenBank2.sol
@@ -10,7 +10,7 @@ contract TokenBank2 is TokenBank{
 
     function tokensReceived(address _sender, uint256 _amount) public returns (bool){
         require(_amount > 0,"must > zero amount");
-        
+        // 调用此方法的用户必须是token合约，否则会出现安全问题
         require(msg.sender == tokenAddress, "can't receive!");
         tokenBalances[_sender] += _amount;
         return true;
